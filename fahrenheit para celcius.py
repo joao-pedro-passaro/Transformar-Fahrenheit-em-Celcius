0b65dda0734c63f1c2fa423e5cda79d2a303795e
João Pedro Passaro <joao.passaro@hotmail.com>
2026-03-22 21:25:12 -0300

    Add files via upload

---
+++ fahrenheit para celcius.py
@@ -0,0 +1,3 @@
+temperatura_f = float(input("Informe a temperatura em Fahrenheit: "))
+Celcius = 5 * ((temperatura_f - 32) / 9)
+print (f"transformado em Celcius, a temperatura é de {Celcius:.2f}°")
